a8adb727070482617c3a298c641d627d26cbd2ab
maysaahourani <maysaahourani@gmail.com>
2021-07-05 14:11:28 +0300

    lab11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import IsLoadingAndError from './IsLoadingAndError';
+import Footer from './Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { withAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/LoginButton';
+import BrowserRouter from './components/BrowserRouter';
+// import Profile from './components/Profile';
+// import LogoutButton from './components/LogoutButton'
+import BestBooks from './BestBooks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    console.log('app', this.props);
+    return(
+      <>
+        <Router>
+          <IsLoadingAndError>
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {
+              this.props.auth0.isAuthenticated ?
+                 < BestBooks />:
+                 <LoginButton/>            
+              
+
+               }
+              </Route>
+              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <BrowserRouter/>
+            </Switch>
+            <Footer />
+          </IsLoadingAndError>
+        </Router>
+      </>
+    );
+  }
 }
 
-export default App;
+export default  withAuth0(App);
---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { withAuth0 } from '@auth0/auth0-react';
+
+
+
+export class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This the Profile page</h1> 
+                {
+                    this.props.auth0.isAuthenticated&&
+                    <>
+                    <h1>{this.props.auth0.user.name}</h1>
+                    <img src={this.props.auth0.user.picture} alt={this.props.auth0.user.name}/>
+                    <h1>{this.props.auth0.user.email}</h1>
+                    </>
+
+                }
+                
+            </div>
+        )
+    }
+}
+
+export default withAuth0(Profile);
